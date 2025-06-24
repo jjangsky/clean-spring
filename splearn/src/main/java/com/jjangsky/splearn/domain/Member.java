a9f67010c589c3d30675b82f64a060b26bc38353
jjangsky <jjangsky@example.com>
2025-06-25 00:25:58 +0900

    feat: Member 도메인에 대한 Testcase 추가

--- splearn/src/main/java/com/jjangsky/splearn/domain/Member.java
+++ splearn/src/main/java/com/jjangsky/splearn/domain/Member.java
@@ -1,5 +1,13 @@
 package com.jjangsky.splearn.domain;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.util.Objects;
+
+import static org.springframework.util.Assert.*;
+
+@Getter
+@ToString
 public class Member {
     private String email;
     private String nickname;
@@ -7,25 +15,29 @@ public class Member {
     private MemberStatus status;
 
     public Member(String email, String nickname, String passwordHash) {
-        this.email = email;
-        this.nickname = nickname;
-        this.passwordHash = passwordHash;
+        this.email = Objects.requireNonNull(email); // 값이 null 이 들어오면 npe 발생
+        this.nickname = Objects.requireNonNull(nickname);
+        this.passwordHash = Objects.requireNonNull(passwordHash);
+
         this.status = MemberStatus.PENDING;
     }
 
-    public String getEmail() {
-        return email;
+    public void activate() {
+        /**
+         * 하단의 방식 처럼 IF 를 사용하여 로직을 처리하는 것도 좋지만
+         * 이러한 자잘한 분기 처리가 계속 쌓이면 결국에는 핵심 비즈니스 로직을 이해하는데 방해가 된다.
+         *
+         * 따라서 가독성이 있게 Assert를 사용하여 상태를 정의 내리는 것도 좋은 방법이다.
+         */
+//        if(status == MemberStatus.PENDING) throw new IllegalArgumentException("PENDING 상태가 아닙니다.");
+        state(status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
+
+        this.status = MemberStatus.ACTIVE;
     }
 
-    public String getNickname() {
-        return nickname;
-    }
+    public void deactivate() {
+        state(status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public MemberStatus getStatus() {
-        return status;
+        this.status = MemberStatus.DEACTIVATED;
     }
 }
--- splearn/src/test/java/com/jjangsky/splearn/domain/MemberTest.java
+++ splearn/src/test/java/com/jjangsky/splearn/domain/MemberTest.java
@@ -1,8 +1,11 @@
 package com.jjangsky.splearn.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 class MemberTest {
     @Test
@@ -11,4 +14,69 @@ class MemberTest {
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
+    @Test
+    void constructorNullCheck() {
+        assertThatThrownBy(() -> new Member(null, "Toby", "secret"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate() {
+        var member = new Member("jjangsky@github.io", "youchan", "secret");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        /**
+         * 이미 활성화 된 회원에 대해서 다시 활성화를 시키는 경우에 대해서 대처하는 테스트 코드
+         * 무의미하지 않겠냐? -> 추후 버그 가능성 농후
+         */
+        var member = new Member("jjangsky@github.io", "youchan", "secret");
+
+        member.activate();
+
+        assertThatThrownBy(() -> {
+            member.activate();
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate() {
+        var member = new Member("jjangsky@github.io", "youchan", "secret");
+
+        member.activate();
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        var member = new Member("jjangsky@github.io", "youchan", "secret");
+
+        assertThatThrownBy(() -> {
+            member.deactivate();
+        }).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(() -> {
+            member.deactivate();
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    /**
+     * 테스트 코드 작성할 때 생각할 것.
+     * 1. Given - when - then을 주석으로 정의하며 규칙을 지켜 개발하는 것도 좋지만
+     *    너무 규칙을 맞출 필요는 없다. 대충 봐도 알만하게 작성해야 함 (초보들이나 bdd 씀)
+     * 2. 간단한 테스트 라면 하나의 테스트 코드에 여러 가지 case를 넣어도 댐
+     * 3. `@Display`로 테스트 케이스에 대한 설명을 적는데 상황을 잘 구분 해야함
+     *     보통 테스트 코드는 한번 작성하고 다시 안보는데 굳이 작성해야하나?
+     *     -> 실패한 Case에 대해서만 재확인 해도 되며 복합적으로 복잡한 Case 에만 추가해도 됨
+     */
 }
